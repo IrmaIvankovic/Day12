29b28b83b7216b1089d25d10d4a849b604e68495
IrmaIvankovic <ivankovicirma@gmail.com>
2021-03-19 23:46:47 +0100

    Day 12

---
+++ ex04/src/index.js
@@ -0,0 +1,24 @@
+function sumFibonacci(num) {
+    if (num == 1) {
+      return 1;
+    }
+    let prevNumber = 0;
+    let currNumber = 1;
+    let result = 0;
+    while (currNumber <= num) {
+      if (currNumber % 2 !== 0) {
+        result += currNumber;
+      }
+      currNumber += prevNumber;
+      prevNumber = currNumber - prevNumber;
+    }
+  
+    return result;
+  }  
+  console.log(sumFibonacci(1));
+  console.log(sumFibonacci(10));
+  console.log(sumFibonacci(20));
+  console.log(sumFibonacci(4));
+  console.log(sumFibonacci(-5));
+  
+  module.exports = sumFibonacci;
